21b9ed877a8a9d453532373954773598161dedcc
chunloklo <clo42@gatech.edu>
2020-07-11 18:04:00 -0400

    beginning of multiplayer. Committed for multi-machine testing

---
+++ Source/BPSummer2020/BPSummer2020.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BPSummer2020.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BPSummer2020, "BPSummer2020" );
